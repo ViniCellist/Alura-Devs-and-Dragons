9511d545ead2ad21577e452f1e9db2c15421e93a
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-20 14:55:31 -0300

    feat: badge update

--- src/modules/Archer.js
+++ src/modules/Archer.js
@@ -1,5 +1,24 @@
 import { Character } from "./character.js";
 
 export class Archer extends Character {
-    
-}
+    job = "Archer";
+    dextery;
+    agility;
+
+    constructor(name, level, hp, mp, dextery, agility) {
+        super(name, level, hp, mp);
+        this.dextery = dextery;
+        this.agility = agility;
+
+    };
+
+    obtainBadge() {
+        if(this.dextery > this.agility) {
+            return `Marksman`;
+        } else if(this.dextery < this.agility) {
+            return `Windwalker`;
+        } else {
+            return `Hunter`;
+        };
+    };
+};

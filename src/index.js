91362ed1365ff49fbdccb8768c3ca5cbbcaae872
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-20 15:08:41 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { Archer } from "./modules/Archer.js";
 import { ArcaneArcher } from "./modules/ArcaneArcher.js";
 import { Warrior } from "./modules/Warrior.js";
 
-const p1 = new Archer('Apollo', 12, 15, 5, 20, 7)
+const p1 = new Archer('Apollo', 12, 15, 5, 20, 7);
 const p2 = new Mage('Magnus', 10, 12, 17, 'Water', 12, 9);
 const p3 = new ArcaneArcher('Finrod', 13, 25, 30, 21, 13, 'Dark', 26, 17);
 const p4 = new Warrior('Alfarr', 12, 37, 12, 20, 21);

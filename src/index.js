78aa09e8dc294dea750563187138f0c616e3c490
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-20 14:55:02 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import { Character } from "./modules/character.js";
 import { CharacterView } from './components/personagem-view.js';
 import { Mage } from "./modules/Mage.js";
+import { Archer } from "./modules/Archer.js";
+import { ArcaneArcher } from "./modules/ArcaneArcher.js";
 
-const mageVinivius = new Mage('Vinicius', 4, 10, 11, 'Fire', 11, 8)
-const mageGabriel = new Mage('Gabriel', 5, 12, 17, 'Water', 12, 9);
+const vinicius = new Archer('Vinicius', 7, 15, 5, 20, 7)
+const gabriel = new Mage('Gabriel', 5, 12, 17, 'Water', 12, 9);
+const fulano = new ArcaneArcher('Fulano', 13, 25, 30, 21, 13, 'Dark', 26, 17);
 
-const chars = [mageVinivius, mageGabriel];
+const chars = [vinicius, gabriel, fulano];
 
 new CharacterView(chars).render();
-
-
-console.log(mageGabriel)

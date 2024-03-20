5410963afc2b59a93bc5dcf8adf64fc6bb91b39c
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-20 13:31:11 -0300

    Update Mage.js

--- src/modules/Mage.js
+++ src/modules/Mage.js
@@ -1,11 +1,65 @@
-export class Mage {
+import { Character } from "./character.js";
+
+export class Mage extends Character{
     element;
     intelligence;
     powerLevel;
+    job = 'Mage';
 
-    constructor(element, intelligence, powerLevel) {
+    constructor(name, level, hp, mp, element, intelligence, powerLevel) {
+        super(name, level, hp, mp)
         this.element = element;
         this.intelligence = intelligence;
         this.powerLevel = powerLevel;
     };
 };
+
+/*
+Elements: Fire, Water, Ground, Wind. Special: Dark, Light, Nature
+
+Habilidades{
+    fire {
+        offensive: FireBall,
+        defensive: FireWall,
+        special: Eruption,
+        passive: Fire Immunity
+    },
+    water {
+        offensive: Water,
+        defensive: WaterWall,
+        special: Tsunami,
+        passive: Waterbreathing
+    },
+    ground {
+        offensive: Spikes,
+        defensive: StoneWall,
+        special: Fissure,
+        passive: Never Lost
+    },
+    lightning {
+        offensive: Thunderbolt,
+        defensive: ThunderCloak,
+        special: Storm,
+        passive: Tracker
+    },
+    dark {
+        offensive: Soul Absorb,
+        defensive: 4th Dimension,
+        special: Blackhole,
+        passive: Night Vision
+    },
+    light {
+        offensive: Judgement,
+        defensive: Holy Protection,
+        special: Punishment,
+        passive: Magic Resistence
+    },
+    nature {
+        offensive: Whip Prison,
+        defensive: Forest Clone,
+        special: Life's cycle,
+        passive: Animal's friend
+    }
+}
+
+*/

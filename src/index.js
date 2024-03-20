ac00442d3125c9870c1309305896333d9d1ffdb5
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-20 10:24:11 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ import { CharacterView } from './components/personagem-view.js';
 
 const vinicius = new Character("Vinicius", 12, 5, 1, "Archer", "Wind");
 
-const personagem = [vinicius]
-new CharacterView(personagem).render()
+const gabriel = new Character("Gabriel", 10, 15, 1, "Mage", "Fire");

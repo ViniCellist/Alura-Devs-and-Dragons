4cb63e661fc16c13e8769cc3f60f667407dee6dd
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-20 12:07:44 -0300

    feat: hp and mp

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import { Character } from "./modules/character.js";
 import { CharacterView } from './components/personagem-view.js';
+import { Mage } from "./modules/Mage.js";
 
-const vinicius = new Character("Vinicius", 1, "Archer");
-const gabriel = new Character("Gabriel", 1, "Mage");
+const vinicius = new Character("Vinicius", 1, "Archer", 10, 5);
+const gabriel = new Character("Gabriel", 1, "Mage", 8, 12);
 
 const chars = [vinicius, gabriel];
 
 new CharacterView(chars).render();
+
+console.log(new Mage('Fire', 21, 7));

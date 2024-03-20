7085c0c046296116422eb93ce53ead3e3847d311
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-20 12:08:12 -0300

    creat: mage class extends

---
+++ src/modules/Mage.js
@@ -0,0 +1,11 @@
+export class Mage {
+    element;
+    intelligence;
+    powerLevel;
+
+    constructor(element, intelligence, powerLevel) {
+        this.element = element;
+        this.intelligence = intelligence;
+        this.powerLevel = powerLevel;
+    };
+};

31eaed3c16655cebcc40d059bee8e2d0b26fb259
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-20 10:53:09 -0300

    fix: reference job error

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { Character } from "./modules/character.js";
 import { CharacterView } from './components/personagem-view.js';
 
-const vinicius = new Character("Vinicius", 12, 5, 1, "Archer", "Wind");
+const vinicius = new Character("Vinicius", 1, "Archer");
+const gabriel = new Character("Gabriel", 1, "Mage");
 
-const gabriel = new Character("Gabriel", 10, 15, 1, "Mage", "Fire");
+const chars = [vinicius, gabriel];
+
+new CharacterView(chars).render();

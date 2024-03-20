41bd433939bc26fec71177180237dc6a6c846fae
Vinicius Duarte <146869828+ViniCellist@users.noreply.github.com>
2024-03-20 00:06:23 -0300

    new bug to fix

--- src/index.js
+++ src/index.js
@@ -1,20 +1,7 @@
 import { Character } from "./modules/character.js";
+import { CharacterView } from './components/personagem-view.js';
 
-const vinicius = new Character();
-vinicius.name = 'Vinicius';
-vinicius.health = 13;
-vinicius.magic = 7;
-vinicius.level = 1;
-vinicius.job = 'Archer';
-vinicius.type = 'Wind';
+const vinicius = new Character("Vinicius", 12, 5, 1, "Archer", "Wind");
 
-const gabriel = new Character();
-gabriel.name = 'Gabriel';
-gabriel.health = 17;
-gabriel.magic = 5;
-gabriel.level = 1;
-gabriel.job = 'Thief';
-gabriel.type = 'Dark';
-
-console.log('Insignia de ' + vinicius.name + ': ' + vinicius.obtainBadge());
-console.log('Insignia de ' + gabriel.name + ': ' + gabriel.obtainBadge());
+const personagem = [vinicius]
+new CharacterView(personagem).render()
--- src/modules/character.js
+++ src/modules/character.js
@@ -6,6 +6,15 @@ export class Character {
     job;
     type;
 
+    constructor(name, health, magic, level, job, type) {
+        this.name = name;
+        this.health = health;
+        this.magic = magic;
+        this.level = level;
+        this.job = job
+        this.type = type;
+    }
+
     obtainBadge() {
         if (this.level >= 5) {
             return `Legendary ${this.type}`;
